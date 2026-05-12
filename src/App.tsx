d2f612562ba85ce2c9b75273bae3138ab5e55966
pranav354 <pranav@tranktechies.com>
2026-05-12 15:14:19 +0530

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Header from "./reuseable-components/Header";
+import Footer from "./reuseable-components/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>{/* <Route path="/" element={<Homepage />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
       <Footer />
     </div>
   );
