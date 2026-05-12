b1b11ca38cea4a0a1e221c02e09e329ba446758d
WELCOME <harsh@tranktechies.com>
2026-05-12 15:07:30 +0530

    fsadsadasd

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
+      <Routes>{/* <Route path="/" element={<Homepage />} /> */}</Routes>
       <Footer />
     </div>
   );
