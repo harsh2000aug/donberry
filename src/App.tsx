178a9ee8864dd3bf028a49d6344541814008df74
WELCOME <harsh@tranktechies.com>
2026-05-12 14:45:46 +0530

    fsad

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import Header from "./reuseable-components/Header";
 import "./App.css";
 import Footer from "./reuseable-components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import donberry from "../images/landing-page.png";
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <section className="gap">
+        <div className="container">
+          <div className="landing-area">
+            <div className="left-side">
+              <img src={donberry} alt="" />
+            </div>
+            <div className="right-side">
+              <p>MASTER OF RESONANCE</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Homepage;
